864eb348018ebdc944eccfd8fac7b6f8ef142d21
Mallikarjun S M <mallikarjunmarabasari@gmail.com>
2026-04-22 09:36:29 +0530

    Updated Copyright policy

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
   return (
     <footer className="bg-gray-900 dark:bg-black border-t border-yellow-400/20 py-8">
       <div className="max-w-7xl mx-auto px-4 text-center text-gray-400">
-        <p>© 2025 EduCompass. Master your learning journey.</p>
+        <p>© 2026 EduCompass. Master your learning journey.</p>
       </div>
     </footer>
   );
